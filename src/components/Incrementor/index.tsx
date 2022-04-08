12cfba934d750c6ca52e69be647b15b0cce82c75
Eduardo Borges <alemaobrk1@gmail.com>
2022-04-07 23:39:20 -0300

    torna botão reativo, formata preço

--- src/components/Incrementor/index.tsx
+++ src/components/Incrementor/index.tsx
@@ -1,25 +1,39 @@
 import { Plus as PlusIcon } from "@styled-icons/boxicons-regular/Plus";
 import { Subtract as SubtractIcon } from "@styled-icons/remix-fill/Subtract";
+import { useState } from "react";
 
 import { Wrapper, IconWrapper, Quantity } from "./styles";
 
 type IncrementorProps = {
   id: number;
   quantity: number;
+  changeQuantityBuy: (typeAction:string) => void;
 };
 
-const Incrementor = ({ id, quantity }: IncrementorProps) => (
+const Incrementor = ({
+  id, 
+  quantity,
+  changeQuantityBuy 
+}: IncrementorProps) => {
+  
+  return (
   <Wrapper>
     <IconWrapper>
-      <SubtractIcon aria-label="Subtract item" />
+      <SubtractIcon 
+        aria-label="Subtract item" 
+        onClick={()=> changeQuantityBuy("sub")}/>
     </IconWrapper>
 
     <Quantity>{quantity}</Quantity>
 
     <IconWrapper>
-      <PlusIcon aria-label="Add item" />
+      <PlusIcon 
+        aria-label="Add item" 
+        onClick={()=> changeQuantityBuy("sum")}
+        />
     </IconWrapper>
   </Wrapper>
-);
+  )
+}
 
 export default Incrementor;
--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,28 +1,47 @@
+import { useState } from "react";
 import Incrementor from "../Incrementor";
 import { Wrapper, Info, Column, Text, WrapperIncrementor } from "./styles";
 
 export type ProductProps = {
   id: number;
   name: string;
-  price: number;
   picture: string;
-};
+  price: number;
+  quantity: number;
+}
 
-const Product = ({ id, name, price, picture }: ProductProps) => (
+const Product = ({ id, name, price, picture, quantity }: ProductProps) => {
+  const [ quantityBuy, setQuantityBuy ] = useState(1)
+
+  let priceTotal = price * quantityBuy;
+  let formattedPrice = priceTotal.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+
+  function changeQuantityBuy (typeAction:string) {
+    if (typeAction === "sub" && quantityBuy > 0 )
+      setQuantityBuy(quantityBuy - 1) 
+    else if (typeAction === "sum")
+      setQuantityBuy(quantityBuy + 1)
+  }
+
+  return (
   <Wrapper>
     <img src={picture} alt={`Imagem de referência ${name}`} />
 
     <Info>
       <Column>
-        <Text>{name}</Text>
-        <Text>{price}</Text>
+        <Text>{ name }</Text>
+        <Text>{ formattedPrice }</Text>
       </Column>
 
       <WrapperIncrementor>
-        <Incrementor id={id} quantity={1} />
+        <Incrementor 
+          id={id} 
+          quantity={quantityBuy}
+          changeQuantityBuy={changeQuantityBuy} />
       </WrapperIncrementor>
     </Info>
   </Wrapper>
-);
+  );
+}
 
 export default Product;
