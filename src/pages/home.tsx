3bf0a8f830d905236668b17ae5d7ad3a84e28d00
Eduardo Borges <alemaobrk1@gmail.com>
2022-04-07 23:38:17 -0300

    conecta com a api, retorna lista de produtos

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,27 +1,48 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Cart from "../components/Cart";
 import { Container } from "../components/Container";
 import Header from "../components/Header";
 import Product, { ProductProps } from "../components/Product";
 
-const data: ProductProps = {
-  id: 1,
-  name: "Product 1",
-  picture:
-    "https://somos.lojaiplace.com.br/wp-content/uploads/2021/04/apple_iphone-12-spring21_purple_04202021.jpg",
-  price: 20.50,
-};
+// const data: ProductProps = {
+//   id: 1,
+//   name: "Product 1",
+//   picture:
+//     "https://somos.lojaiplace.com.br/wp-content/uploads/2021/04/apple_iphone-12-spring21_purple_04202021.jpg",
+//   price: 20.50,
+//   quantity: 2
+// }
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [ loading, setLoading ] = useState(true)
+  const [ dataArray, setDataArray ] = useState ([])
 
+  useEffect(() => { buscaProdutos() }, [])
+  useEffect(() => { setLoading(false) }, [dataArray])
+
+  async function buscaProdutos () {
+    const url = " http://localhost:3001/products"
+    const response = await axios.get( url )
+    // console.log(response.data)
+    setDataArray(response.data)
+  }
+    
   return (
+    loading ?
+    <div> </div> :
     <>
       <Header setIsOpen={setIsOpen} />
-      <Container>
-        <Product {...data} />
-        <Cart isOpen={isOpen} setIsOpen={setIsOpen} />
-      </Container>
+      {
+        dataArray.map((product) => {
+          return (
+          <Container>
+            <Product {...product} />
+            <Cart isOpen={isOpen} setIsOpen={setIsOpen} />
+          </Container> )
+        })
+      }
     </>
   );
 };
