0168e954146c27d8f3dcf57258bb227ad5659ea1
Eduardo Borges <alemaobrk1@gmail.com>
2022-04-09 14:39:37 -0300

    altera incrementor para setar quantidade no carrinho

--- src/components/Incrementor/index.tsx
+++ src/components/Incrementor/index.tsx
@@ -1,27 +1,31 @@
 import { Plus as PlusIcon } from "@styled-icons/boxicons-regular/Plus";
 import { Subtract as SubtractIcon } from "@styled-icons/remix-fill/Subtract";
 import { useState } from "react";
+import { useCart, useProduct } from "../../state/globalState";
 
 import { Wrapper, IconWrapper, Quantity } from "./styles";
 
 type IncrementorProps = {
   id: number;
   quantity: number;
-  changeQuantityBuy: (typeAction:string) => void;
+  changeQuantityBuy: (typeAction:string, quantity: number) => void;
 };
 
 const Incrementor = ({
   id, 
   quantity,
-  changeQuantityBuy 
+  changeQuantityBuy, 
 }: IncrementorProps) => {
-  
+
+  // const {cart} = useCart()
+  // console.log("inc", cart)
+
   return (
   <Wrapper>
     <IconWrapper>
       <SubtractIcon 
         aria-label="Subtract item" 
-        onClick={()=> changeQuantityBuy("sub")}/>
+        onClick={()=> changeQuantityBuy("sub", quantity-1)}/>
     </IconWrapper>
 
     <Quantity>{quantity}</Quantity>
@@ -29,7 +33,7 @@ const Incrementor = ({
     <IconWrapper>
       <PlusIcon 
         aria-label="Add item" 
-        onClick={()=> changeQuantityBuy("sum")}
+        onClick={()=> changeQuantityBuy("sum", quantity+1)}
         />
     </IconWrapper>
   </Wrapper>
