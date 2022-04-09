27d9c818a95f4564d9e9f31a50614ce271508560
Eduardo Borges <alemaobrk1@gmail.com>
2022-04-09 14:32:56 -0300

    altera a chamada à API para obter os produtos

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,34 +1,18 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
 import Cart from "../components/Cart";
 import { Container } from "../components/Container";
 import Header from "../components/Header";
-import Product, { ProductProps } from "../components/Product";
-
-// const data: ProductProps = {
-//   id: 1,
-//   name: "Product 1",
-//   picture:
-//     "https://somos.lojaiplace.com.br/wp-content/uploads/2021/04/apple_iphone-12-spring21_purple_04202021.jpg",
-//   price: 20.50,
-//   quantity: 2
-// }
+import Product from "../components/Product";
+import { useProduct } from "../state/globalState";
 
 const Home = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [ isOpen, setIsOpen ] = useState(false);
   const [ loading, setLoading ] = useState(true)
-  const [ dataArray, setDataArray ] = useState ([])
 
-  useEffect(() => { buscaProdutos() }, [])
+  const {dataArray, setDataArray} = useProduct()
+
+  useEffect(() => { setDataArray() }, [])
   useEffect(() => { setLoading(false) }, [dataArray])
-
-  async function buscaProdutos () {
-    const url = " http://localhost:3001/products"
-    const response = await axios.get( url )
-    // console.log(response.data)
-    setDataArray(response.data)
-  }
-    
   return (
     loading ?
     <div> </div> :
@@ -48,3 +32,7 @@ const Home = () => {
 };
 
 export default Home;
+function useProducts() {
+  throw new Error("Function not implemented.");
+} // o que é iso e porque está aqui ?
+
