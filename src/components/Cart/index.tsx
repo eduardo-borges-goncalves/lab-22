174d2ca79f8bd86f3641ae2217b6211396316955
Eduardo Borges <alemaobrk1@gmail.com>
2022-04-10 01:26:47 -0300

    altera lógica de construção do preço total do cart

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -7,7 +7,6 @@ import Typography from "../Typography";
 import { Wrapper, Subtotal, Header } from "./styles";
 import { useCart } from "../../state/globalState";
 import Product from "../Product";
-import { CartProduct } from "../CartProduct";
 
 export type MenuPaymentProps = {
   isOpen: boolean;
@@ -22,80 +21,20 @@ export type MenuPaymentProps = {
  * - Incrementador
  */
 
-/*
- * 
- * passar para um array o preço com o id
- * se id já existente, excluir e atualizar o preço 
- * formatar o preço 
- * porque está pegando trezentas vezes?
-*/
-
-type Total = {
-  id: number, 
-  precoTotal: number,
-  price: number, 
-  name: string, 
-  picture: string,
-  quantity: number,
-}
-
-export type Formatted = {
-  id: number, 
-  precoFormado: string, 
-  name: string, 
-  picture: string,
-  precoTotalNumerico: number,
-  price: number, 
-  quantity: number,
-}
-
-// refatorar
-// ter apenas um total
-// lista de produtos no carrinho 
-// conectar o incrementor da home com o do carrinho
-
 const MenuPayment = ({ isOpen, setIsOpen }: MenuPaymentProps) => {
-   
-  const [ formattedPrice, setFormattedPrice ] = useState<Formatted[]> ([])
-  const [ total, setTotal ] = useState<Total[]> ([])
+  const [totalPrice, setTotalPrice] = useState(0)
 
   const { cart } = useCart()
 
-  useEffect( () => {   
-    cart.map( car => {
-
-      let arnaldo:Total[] = total.filter(value => car.id !== value.id) 
-      setTotal( [ ...arnaldo, {
-        id: car.id,
-        price: car.price,
-        precoTotal: car.price * car.quantity, 
-        picture: car.picture, 
-        name: car.name, 
-        quantity: car.quantity
-      }] ) 
-      
+  useEffect(() => {
+    let cumulatedPrice = 0
+    cart.map(car => {
+      cumulatedPrice = car.price * car.quantity + cumulatedPrice
+      setTotalPrice(cumulatedPrice)
     })
-  } ,[cart])
+  }, [cart])
 
-  useEffect( () => {  
-    let copiaFormatted:Formatted[] = []
-
-      total.map( value => {
-        copiaFormatted.push({
-          id: value.id, 
-          precoFormado: value.precoTotal.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'}), 
-          name: value.name, 
-          picture: value.picture,
-          price: value.price, 
-          precoTotalNumerico: value.precoTotal,
-          quantity: value.quantity,
-        })
-        setFormattedPrice(copiaFormatted) 
-      })
- 
-  }, [total])
-  
-  console.log("oi Deus to cansado", formattedPrice)
+  let formattedPrice = totalPrice.toLocaleString("pt-br", { style: 'currency', currency: "BRL" })
 
   return (
     <Wrapper isOpen={isOpen}>
@@ -105,38 +44,35 @@ const MenuPayment = ({ isOpen, setIsOpen }: MenuPaymentProps) => {
         </Typography>
         <CloseOutline onClick={() => setIsOpen(false)} />
       </Header>
-      {
-        formattedPrice.length > 0 && cart[0].quantity > 0 ? 
-          <>
-            { 
-              formattedPrice.map( productPrice => {
-                return (
-                  <>
-                    <Subtotal>
-                      <CartProduct { ...productPrice} />
-                    </Subtotal>
-                    <Subtotal>
-                      <Typography level={5} size="large" fontWeight={600}>
-                        Total
-                      </Typography> 
-                      <Typography>{ productPrice.precoFormado }
-                      </Typography>
-                    </Subtotal>
-                  </>
-                )
-              })
-            }
-          </>
-          :
-            <Subtotal>
-              <Typography level={5} size="large" fontWeight={600}>
-                Total
-              </Typography>
-              <Typography> R$ 0,00 </Typography>
-            </Subtotal>
-      } 
+      { cart.length > 0 ?
+        <>
+        { cart.map(product => {  
+            if (product.quantity !== 0) {
+              return (
+                <Subtotal>
+                  <Product {...product} />
+                </Subtotal> 
+              )
+            } 
+          })
+        }
+        <Subtotal>
+          <Typography level={5} size="large" fontWeight={600}>
+            Total
+          </Typography>
+          <Typography>{formattedPrice}</Typography>
+        </Subtotal>
+        </>
+        :
+        <Subtotal>
+          <Typography level={5} size="large" fontWeight={600}>
+            Total
+          </Typography>
+          <Typography> R$ 0,00 </Typography>
+        </Subtotal>
+      }
       <Button fullWidth>Finalizar compra</Button>
-    </Wrapper> 
+    </Wrapper>
   );
 }
 
