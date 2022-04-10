7586ec1d52fde48894ee74bc3e403fcb361c430a
Eduardo Borges <alemaobrk1@gmail.com>
2022-04-10 01:24:59 -0300

    altera quantidade mostrada e torna dados do carrinho e home compatíveis

--- src/components/Incrementor/index.tsx
+++ src/components/Incrementor/index.tsx
@@ -1,39 +1,50 @@
 import { Plus as PlusIcon } from "@styled-icons/boxicons-regular/Plus";
 import { Subtract as SubtractIcon } from "@styled-icons/remix-fill/Subtract";
-import { useState } from "react";
-import { useCart, useProduct } from "../../state/globalState";
+import { useEffect, useState } from "react";
+import { useCart, useQuantity } from "../../state/globalState";
 
 import { Wrapper, IconWrapper, Quantity } from "./styles";
 
 type IncrementorProps = {
   id: number;
-  quantity: number;
-  changeQuantityBuy: (typeAction:string, quantity: number) => void;
+  setProductInCart: (quantity: number) => void;
 };
 
 const Incrementor = ({
   id, 
-  quantity,
-  changeQuantityBuy, 
+  setProductInCart, 
 }: IncrementorProps) => {
+  const [incrementorQuantity, setIncrementorQuantity] = useState(0)
 
-  // const {cart} = useCart()
-  // console.log("inc", cart)
+  const {quantity, setQuantity} = useQuantity()
+
+  useEffect(() => {
+    let quantityToIncrementor = quantity.filter(item=> item.id === id)
+    if (quantityToIncrementor.length > 0) {
+      setIncrementorQuantity(quantityToIncrementor[0].quantityPerProduct)
+      setProductInCart( quantityToIncrementor[0].quantityPerProduct )
+    }
+  }, [quantity])
+
+  function handleSetQuantity(type: string) {
+    type === "sub" && incrementorQuantity > 0 && setQuantity(id, incrementorQuantity - 1); 
+    type === "sum" && setQuantity(id, incrementorQuantity + 1);
+  }
 
   return (
   <Wrapper>
     <IconWrapper>
       <SubtractIcon 
         aria-label="Subtract item" 
-        onClick={()=> changeQuantityBuy("sub", quantity-1)}/>
+        onClick={()=> handleSetQuantity("sub")}/>
     </IconWrapper>
 
-    <Quantity>{quantity}</Quantity>
+    <Quantity>{incrementorQuantity}</Quantity>
 
     <IconWrapper>
       <PlusIcon 
         aria-label="Add item" 
-        onClick={()=> changeQuantityBuy("sum", quantity+1)}
+        onClick={()=> handleSetQuantity("sum") }
         />
     </IconWrapper>
   </Wrapper>
